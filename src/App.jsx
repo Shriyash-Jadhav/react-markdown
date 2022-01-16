80df304a7f7d60286b52690e68c18dd54490042d
Shriyash Jadhav <shriyashjadhav370@gmail.com>
2022-01-16 20:44:23 +0530

    feat: project init

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Badge } from "react-bootstrap";
+import { marked } from "marked";
+import "./App.css";
+const App2 = () => {
+	const [state, setstate] = useState("");
+
+	const updateMarkdown = (markdown) => setstate(markdown);
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="row mt-4">
+					<div className="col text-center">
+						<h1>
+							{" "}
+							<Badge className="text-align-center" bg="dark">
+								Markdown Previewer
+							</Badge>
+						</h1>
+					</div>
+				</div>
+
+				<div className="row mt-4">
+					<div className="col-md-6">
+						{" "}
+						<div className="col text-center">
+							<h4>
+								<Badge className="text-align-center" bg="dark">
+									Markdown Input
+								</Badge>
+							</h4>
+						</div>
+						<div>
+							<textarea
+								className="input-style"
+								value={state}
+								onChange={(e) => {
+									updateMarkdown(e.target.value);
+								}}
+							></textarea>
+						</div>
+					</div>
+
+					<div className="col-md-6">
+						{" "}
+						<div className="col ">
+							<h4>
+								<Badge className="text-align-center" bg="dark">
+									Preview
+								</Badge>
+							</h4>
+						</div>
+						<div
+							className="output-style"
+							dangerouslySetInnerHTML={{
+								__html: marked(state),
+							}}
+						></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App2;
